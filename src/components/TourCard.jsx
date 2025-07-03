c6cb37b02f7e57388234a75acdcfdfbc32dbbd5d
Meri Tsaturyan <meritsaturyan@192.168.27.3>
2025-07-03 15:32:28 +0400

    Добавил basePath в TourCard.jsx

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -90,7 +90,8 @@ const TourCard = ({ tour }) => {
   return (
     <Link to={`/tour/${tour.id}`} style={{ textDecoration: "none", color: "inherit" }}>
       <Card>
-        <Image src={`${basePath}${tour.image}`} alt={tour.title[lang]} />
+      <Image src={`${basePath}${tour.image}`} alt={tour.title[lang]} /> 
+
 
 
         <Info>
