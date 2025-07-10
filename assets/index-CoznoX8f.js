e2f39b6f4624b8c747c84d3f30431b15f09c66be
Meri Tsaturyan <meritsaturyan@192.168.27.3>
2025-07-10 19:06:53 +0400

    Updates

--- assets/index-CoznoX8f.js
+++ assets/index-CoznoX8f.js
@@ -526,10 +526,9 @@ Please change the parent <Route path="${C}"> to <Route path="${C==="/"?"*":`${C}
   width: 100%;
   margin-bottom: 12px;
   padding: 12px;
-  height: 50px;
-  font-size: 16px;
   border: none;
   border-radius: 6px;
+  font-size: 16px;
   background-color: white;
   box-shadow: 0 0 0 1px #ccc inset;
 
@@ -539,7 +538,6 @@ Please change the parent <Route path="${C}"> to <Route path="${C==="/"?"*":`${C}
   }
 `,a2=J.button`
   width: 100%;
-  height: 50px; /* чтобы совпадало с Input */
   padding: 12px;
   background-color: #009344;
   color: white;
